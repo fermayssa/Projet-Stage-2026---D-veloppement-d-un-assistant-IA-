d1523243e3a17cc7dffd1938164e0eb3d5960f04
fermayssa <ferjani.mayssa1@esprit.tn>
2026-06-29 10:09:24 +0100

    Assemblage final — Modification du fichier frontend/src/app/app.ts

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { UploadComponent } from './components/upload/upload';
+import { ChatComponent } from './components/chat/chat';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [UploadComponent, ChatComponent],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrls: ['./app.scss']
 })
 export class App {
-  protected readonly title = signal('frontend');
+  title = 'RAG Assistant';
 }
